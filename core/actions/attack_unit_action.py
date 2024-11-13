6511114c66a24df112cb18d1b4d2a963960f87c8
VoxJickaray <mat.chauviere@gmail.com>
2024-11-13 16:03:42 +0100

    Adding AttackUnitAction and AttackBuildingAction and the corresponding tests

---
+++ core/actions/attack_unit_action.py
@@ -0,0 +1,23 @@
+from core.actions import Action
+from core.units import Unit
+import math
+from datetime import datetime, timedelta
+
+
+class AttackUnitAction(Action):
+
+    __attacking_unit : Unit
+    __attacked_unit : Unit
+
+    def __init__(self, attacking_unit : Unit, attacked_unit : Unit):
+        self.__attacking_unit = attacking_unit
+        self.__attacked_unit = attacked_unit
+        super().__init__()
+
+
+    def do_action(self)->bool:
+        if (math.sqrt((self.__attacked_unit.get_position().get_x() - self.__attacking_unit.get_position().get_x())*(self.__attacked_unit.get_position().get_x() - self.__attacking_unit.get_position().get_x()) +(self.__attacked_unit.get_position().get_y() -self.__attacking_unit.get_position().get_y())*(self.__attacked_unit.get_position().get_y() -self.__attacking_unit.get_position().get_y()) )<= self.__attacking_unit.range):
+            if ((self.get_new_time() - self.get_old_time()) > timedelta(seconds=self.__attacking_unit.attack_speed)):
+                self.__attacked_unit.remove_health_points(self.__attacking_unit.damage)
+
+        return True
---
+++ tests/Actions/attacks/test_attack_actions.py
@@ -0,0 +1,106 @@
+from datetime import timedelta
+
+import pytest
+
+from core.actions.attack_building_action import AttackBuildingAction
+from core.actions.attack_unit_action import AttackUnitAction
+from core.buildings import Building
+from core.map import Map
+from core.players import Player
+from core.position import Position
+from core.resource import Resource
+from core.units import Unit
+
+
+@pytest.fixture
+def my_attack_building_action()->AttackBuildingAction:
+    my_map= Map(120,120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(47, 80)
+    my_cost = Resource(135, 0, 0)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza","blue")
+    my_house = Building(100, 4, 200, my_pos, 4, False, my_cost,my_player)
+    my_unit = Unit(my_other_player,my_map,my_other_pos,2,100,20,1.2,3,15)
+    return AttackBuildingAction(my_unit, my_house)
+
+@pytest.fixture
+def my_attack_unit_action()->AttackUnitAction:
+    my_map= Map(120,120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(47, 80)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza","blue")
+    my_other_unit = Unit(my_player, my_map, my_pos, 2, 100, 20, 11.2,3,15)
+    my_unit = Unit(my_other_player,my_map,my_other_pos,2,100,20,1.2,3,15)
+    return AttackUnitAction(my_unit,my_other_unit )
+
+
+def test_initialisation_attack_building(my_attack_building_action) :
+    assert (my_attack_building_action.get_new_time() - my_attack_building_action.get_old_time() ) >= timedelta(seconds = 80)
+
+def test_before_and_after_action_attack_building(my_attack_building_action) :
+    my_attack_building_action.before_action()
+    my_attack_building_action.after_action()
+    assert (my_attack_building_action.get_new_time() - my_attack_building_action.get_old_time() ) < timedelta(seconds = 2)
+
+def test_do_action_attack_building_sufficient_range() :
+    my_map= Map(120,120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(47, 80)
+    my_cost = Resource(135, 0, 0)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza","blue")
+    my_house = Building(100, 4, 200, my_pos, 4, False, my_cost,my_player)
+    my_unit = Unit(my_other_player,my_map,my_other_pos,2,100,20,1.2,3,15)
+    aba = AttackBuildingAction(my_unit, my_house)
+    aba.do_action()
+    assert my_house.get_health_points() == 180
+
+def test_do_action_attack_building_insufficient_range() :
+    my_map= Map(120,120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(55, 80)
+    my_cost = Resource(135, 0, 0)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza","blue")
+    my_house = Building(100, 4, 200, my_pos, 4, False, my_cost,my_player)
+    my_unit = Unit(my_other_player,my_map,my_other_pos,2,100,20,1.2,3,15)
+    aba = AttackBuildingAction(my_unit, my_house)
+    aba.do_action()
+    assert my_house.get_health_points() == 200
+
+
+
+def test_initialisation_attack_unit(my_attack_unit_action) :
+    assert (my_attack_unit_action.get_new_time() - my_attack_unit_action.get_old_time() ) >= timedelta(seconds = 80)
+
+def test_before_and_after_action_attack_unit(my_attack_unit_action) :
+    my_attack_unit_action.before_action()
+    my_attack_unit_action.after_action()
+    assert (my_attack_unit_action.get_new_time() - my_attack_unit_action.get_old_time() ) < timedelta(seconds = 2)
+
+
+def test_do_action_attack_unit_sufficient_range():
+    my_map = Map(120, 120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(47, 80)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza", "blue")
+    my_other_unit = Unit(my_player, my_map, my_pos, 2, 100, 20, 11.2, 3, 15)
+    my_unit = Unit(my_other_player, my_map, my_other_pos, 2, 100, 20, 1.2, 3, 15)
+    aua = AttackUnitAction(my_unit, my_other_unit)
+    aua.do_action()
+    assert my_other_unit.get_health_points() == 80
+
+def test_do_action_attack_unit_insufficient_range():
+    my_map = Map(120, 120)
+    my_pos = Position(45, 80)
+    my_other_pos = Position(55, 80)
+    my_player = Player("Leopold", "red")
+    my_other_player = Player("Querza", "blue")
+    my_other_unit = Unit(my_player, my_map, my_pos, 2, 100, 20, 11.2, 3, 15)
+    my_unit = Unit(my_other_player, my_map, my_other_pos, 2, 100, 20, 1.2, 3, 15)
+    aua = AttackUnitAction(my_unit, my_other_unit)
+    aua.do_action()
+    assert my_other_unit.get_health_points() == 100
