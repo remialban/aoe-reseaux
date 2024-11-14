f93e9ca72607efc05166a069c85e24cf98989675
VoxJickaray <mat.chauviere@gmail.com>
2024-11-14 12:05:50 +0100

    For the actions including an attacking unit, used getters to get the damage value of the unit.

--- core/actions/attack_building_action.py
+++ core/actions/attack_building_action.py
@@ -22,6 +22,6 @@ class AttackBuildingAction(Action):
     def do_action(self)->bool:
         if (math.sqrt((self.__attacked_building.get_position().get_x() - self.__attacking_unit.get_position().get_x())*(self.__attacked_building.get_position().get_x() - self.__attacking_unit.get_position().get_x()) +(self.__attacked_building.get_position().get_y() -self.__attacking_unit.get_position().get_y())*(self.__attacked_building.get_position().get_y() -self.__attacking_unit.get_position().get_y()) )<= self.__attacking_unit.range):
             if ((self.get_new_time() - self.get_old_time()) > timedelta(seconds = self.__attacking_unit.attack_speed)):
-                self.__attacked_building.remove_health_points(self.__attacking_unit.damage)
+                self.__attacked_building.remove_health_points(self.__attacking_unit.get_damage())
 
         return True
--- core/actions/attack_unit_action.py
+++ core/actions/attack_unit_action.py
@@ -18,6 +18,6 @@ class AttackUnitAction(Action):
     def do_action(self)->bool:
         if (math.sqrt((self.__attacked_unit.get_position().get_x() - self.__attacking_unit.get_position().get_x())*(self.__attacked_unit.get_position().get_x() - self.__attacking_unit.get_position().get_x()) +(self.__attacked_unit.get_position().get_y() -self.__attacking_unit.get_position().get_y())*(self.__attacked_unit.get_position().get_y() -self.__attacking_unit.get_position().get_y()) )<= self.__attacking_unit.range):
             if ((self.get_new_time() - self.get_old_time()) > timedelta(seconds=self.__attacking_unit.attack_speed)):
-                self.__attacked_unit.remove_health_points(self.__attacking_unit.damage)
+                self.__attacked_unit.remove_health_points(self.__attacking_unit.get_damage())
 
         return True
