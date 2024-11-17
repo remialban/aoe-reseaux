c0a018f1ca2fa875a6902c4c96d80c93727e2fe4
Rémi ALBAN <remi.alban@insa-cvl.fr>
2024-11-17 14:44:42 +0100

    Resolve problem in Player class

--- core/players/__init__.py
+++ core/players/__init__.py
@@ -2,11 +2,11 @@ from core.resource import Resource
 
 
 class Player:
-    def __init__(self, name: str, color: str):
+    def __init__(self, name: str, color: str, resource: Resource = Resource(0,0,0)):
         self.name: str = name
         self.color: str = color
         self.max_number_units: int = 0
-        self.stock: Resource = Resource()
+        self.stock: Resource = resource
 
 
     def get_color(self) -> str:
