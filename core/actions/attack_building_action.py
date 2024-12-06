24a208d3429bca63c179b20fa24c70687ac04359
Rémi ALBAN <remi.alban@insa-cvl.fr>
2024-12-06 10:56:23 +0100

    Fix problem about the returned value in the action of AttackBuildingAction + Fix a problem of calculation of distance + refactoring of code

--- core/actions/attack_building_action.py
+++ core/actions/attack_building_action.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 from core.actions import Action
 from core.buildings import Building
+from core.position import Position
 from core.units import Unit
 
 
@@ -17,11 +18,16 @@ class AttackBuildingAction(Action):
         super().__init__()
 
 
+    def distance_between_position_and_units(self, position: Position, unit: Unit):
+        return math.sqrt((unit.get_position().get_x() - position.get_x())**2 +(unit.get_position().get_y() - position.get_y())**2 )
+
+    def distance(self):
+        return min(self.distance_between_position_and_units(Position(x, y), self.__attacking_unit) for x in range(int(self.__attacked_building.get_position().get_x()), int(self.__attacked_building.get_position().get_x() + self.__attacked_building.get_width())) for y in range(int(self.__attacked_building.get_position().get_y()), int(self.__attacked_building.get_position().get_y() + self.__attacked_building.get_height())))
 
 
     def do_action(self)->bool:
-        if (math.sqrt((self.__attacked_building.get_position().get_x() - self.__attacking_unit.get_position().get_x())*(self.__attacked_building.get_position().get_x() - self.__attacking_unit.get_position().get_x()) +(self.__attacked_building.get_position().get_y() -self.__attacking_unit.get_position().get_y())*(self.__attacked_building.get_position().get_y() -self.__attacking_unit.get_position().get_y()) )<= self.__attacking_unit.range):
-            if ((self.get_new_time() - self.get_old_time()) > timedelta(seconds = self.__attacking_unit.attack_speed)):
+        if self.distance() <= self.__attacking_unit.range:
+            if (self.get_new_time() - self.get_old_time()) > timedelta(seconds = self.__attacking_unit.attack_speed):
                 self.__attacked_building.remove_health_points(self.__attacking_unit.get_damage())
 
-        return True
+        return self.__attacked_building.get_health_points() <= 0
