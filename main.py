23a6825c9ff90f234f77eb2687f5c9d97f847cde
Rémi Alban <remialbandeveloper@gmail.com>
2025-03-23 14:41:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from core import Player, TownCenter, AI
 from core.position import Position
 from core.resource import Resource
 from core.units.archer import Archer
+from network.receiver import Receiver
 from network.sender import Sender
 from network.state import State
 from tests.test_resource import resource
@@ -18,13 +19,14 @@ def main():
     UIManager.add_ui(UIList.CLI, CLI())
 
     UIManager.add_ui(UIList.GUI, GUI())
-
+    Receiver.init(UIManager)
     #UIManager.change_ui(UIList.MENU)
+
+
     UIManager.loop()
 
 if __name__ == "__main__":
     State.set_receiving(False)
-
     # p = AI("test", "red")
     # State.set_receiving(False)
     #
